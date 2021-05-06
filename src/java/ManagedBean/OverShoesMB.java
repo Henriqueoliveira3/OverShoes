c1ad06a5a76be603e920bbdaf1f3927ba0cebaa0
Henrique de Oliveira <henrique_oliveira3@estudante.sc.senai.br>
2021-05-06 13:03:43 -0300

    Páginas necessárias e css

---
+++ src/java/ManagedBean/OverShoesMB.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ManagedBean;
+
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author Henrique
+ */
+@Named(value = "overShoesMB")
+@RequestScoped
+public class OverShoesMB {
+
+    /**
+     * Creates a new instance of OverShoesMB
+     */
+    public OverShoesMB() {
+    }
+    public String getPaginaSobre(){
+      return "sobre";
+    }
+    public String getPaginaCadastro(){
+      return "cadastro";
+    }
+    public String getPaginaLogin(){
+        return "login";
+    }
+    public String getPaginaInicial(){
+        return "index";
+    }
+    public String getPaginaIdeias(){
+        return "ideia";
+    }
+    public String getPaginaProdutos(){
+        return "produtos";
+    }
+    public String getPaginaSalgado(){
+        return "salgado";
+    }
+    public String getPaginaDoce(){
+        return "doce";
+    }
+    public String getPaginaBebida(){
+        return "bebida";
+    }
+    
+}
