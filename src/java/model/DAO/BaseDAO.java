256b849a6f1a23f729a731cd67cadfc69165ac6a
cwsez <cwsez@host.docker.internal>
2021-05-11 13:35:37 -0300

    Entidades e Base DAO

--- src/java/model/DAO/BaseDAO.java
+++ src/java/model/DAO/BaseDAO.java
@@ -1,35 +1,42 @@
-package model.DAO;
+package model.dao;
 
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 
-
-public class BaseDAO<T> {
-    private EntityManager entityManager;
-
-    public EntityManager getEntityManager(){
-
-        if(entityManager == null){
-
-            entityManager = Persistence.createEntityManagerFactory("OverShoesPU").createEntityManager();
-        }
-        return entityManager;
-    }
-
-    public T salvar(T objeto)
+public class BaseDao <T>
+{
+    private static EntityManager INSTANCE;
+    
+    public static EntityManager getEntityManager()
     {
+        if(INSTANCE == null)
+        {
+           INSTANCE = Persistence.createEntityManagerFactory("").createEntityManager();
+        }
+         
+        return INSTANCE;
+    }
+    
+    public T save(T object){
+        
       try
-      {
-        getEntityManager().getTransaction().begin();
-        objeto = getEntityManager().merge(objeto);
-        getEntityManager().getTransaction().commit();
-      }
-      catch(Exception e)
-      {
-          System.out.println("Erro: " + e.getLocalizedMessage());
-          getEntityManager().getTransaction().rollback();
-          return null;
-      }
-      return objeto;
+        {
+           getEntityManager().getTransaction().begin();
+           object = getEntityManager().merge(object);
+           getEntityManager().getTransaction().commit();
+        }
+        catch(Exception e)
+        {
+            System.out.println("Erro: " + e.getLocalizedMessage());
+            getEntityManager().getTransaction().rollback();
+            return null;
+        }
+        return object;
+    }
+    
+     public List<T> getAllResults (String query)
+    {
+        return getEntityManager().createNamedQuery(query).getResultList();
     }
 }
